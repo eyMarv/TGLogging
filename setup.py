97cbe302be63cfc0b8ab07fcf1ed0b52b0d47aee
Marvin <eymarv07@gmail.com>
2023-12-15 18:22:05 +0100

    Bump

--- setup.py
+++ setup.py
@@ -5,23 +5,23 @@ with open("README.md", encoding="utf-8") as f:
 
 setuptools.setup(
     name="tglogging",
-    version="0.0.7",
+    version="0.0.9",
     author="subinps + bsxcs",
     description="A python package to stream your app logs to a telegram chat in realtime.",
     long_description=readme,
     long_description_content_type="text/markdown",
     project_urls={
-        "Tracker": "https://github.com/subinps/tglogging/issues",
-        "Source": "https://github.com/subinps/tglogging",
+        "Tracker": "https://github.com/eyMarv/tglogging/issues",
+        "Source": "https://github.com/eyMarv/tglogging",
     },
     license="MIT",
-    url="https://github.com/subinps/tglogging",
+    url="https://github.com/eyMarv/tglogging",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=["aiohttp"],
 )
